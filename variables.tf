ac110f78b0073ae9c959ea30287416ff2e2a324c
Adam <adam.rybczynski14@gmail.com>
2026-02-07 22:41:48 +0100

    Initiated AZ2 project

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  type        = string
+  default     = "westeurope"
+  description = "Azure region"
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = "az1-rg"
+  description = "Resource Group name"
+}
